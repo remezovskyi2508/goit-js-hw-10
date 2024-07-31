50f48edb1749d63820feb18335a179df9f12fd43
Anton Remezovskyi <antonremezovskyi@gmail.com>
2024-07-31 17:44:23 +0300

    start my task-1.js

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,3 +1,8 @@
 import flatpickr from 'flatpickr';
 // Додатковий імпорт стилів
 import 'flatpickr/dist/flatpickr.min.css';
+
+flatpickr('#datetime-picker', {
+  enableTime: true,
+  dateFormat: 'Y-m-d H:i',
+});
